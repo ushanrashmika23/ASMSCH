4a417d4d7d935672f88cce6589baea8ab09a206d
ushanrashmika23 <ushanrashmika23@gmail.com>
2023-05-07 20:11:56 +0530

    .

---
+++ js/main.js
@@ -0,0 +1,20 @@
+window.addEventListener("scroll",function(){
+
+    let header=this.document.querySelector("header");
+    header.classList.toggle("sticky",this.window.scrollY>0);
+});
+
+$(function() {
+    $('a[href*=\\#]:not([href=\\#])').on('click', function() {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.substr(1) + ']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 750);
+            return false;
+        }
+    });
+});
+
+//$('body').scrollTo('#news');
